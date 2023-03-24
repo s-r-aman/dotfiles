6f99d3058e0cfc33c584c396c7d6774132e818c2
SR Aman <aman.sr@kubric.io>
2023-03-25 02:33:59 +0530

    instal utils

---
+++ scripts/utilities.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Script to install mac os utilities
+echo "INSTALLING UTILITIES"
+# to install
+# shottr
+brew install --cask shottr
+# openin
+brew install --cask openin
+# fig
+brew install --cask fig
+# iStatsMenu
+brew install --cask istat-menus
+# alt tab
+brew install --cask alt-tab
+# raycast
+brew install --cask raycast
+# karabiner elements
+brew install --cask karabiner-elements
+
+# install mas
+brew install --cask karabiner-elements
+
+# hidden bar (to hid macos menu bar onw ill)
+mas install 1452453066
+# dropover
+mas insall 1355679052
+# pastepal
+mas install 1503446680
