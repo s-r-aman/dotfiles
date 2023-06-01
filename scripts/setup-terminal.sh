b332e63168fb4a61c220729af809634983f16f6a
SR Aman <aman.sr@kubric.io>
2023-06-01 17:36:26 +0530

    UPDATE

---
+++ scripts/setup-terminal.sh
@@ -0,0 +1,12 @@
+# install ZSH
+brew install zsh
+
+# set the zsh as default terminal
+chsh -s /opt/homebrew/bin/zsh
+
+# isntall omz
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+# install oh my zsh
+ brew install startship
